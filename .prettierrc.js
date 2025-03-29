312b5ef4834a11337ed20f02cf8c438e9d3b0efc
Zhipeng Ye <yezhipeng99@foxmail.com>
2025-03-29 15:21:47 +1100

    brand new project

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  bracketSpacing: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
-  printWidth: 100,
-  tabWidth: 2,
-  semi: true,
-}; 
--- src/api/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import Constants from "expo-constants";
-
-// 在实际开发中，这些值应该从环境变量或 Constants.manifest.extra 获取
-const supabaseUrl = "YOUR_SUPABASE_URL";
-const supabaseAnonKey = "YOUR_SUPABASE_ANON_KEY";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
